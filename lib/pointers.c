ebae12f0cb6dd09623858c2d579529b54e871ee6
Aaron <aaron.ortega15@outlook.es>
2022-08-29 09:28:40 -0500

    correct Implementation of BubbleSort

--- lib/pointers.c
+++ lib/pointers.c
@@ -7,16 +7,18 @@
 
 int bubble_sort(int *collection, int count)
 {
+    int swapMade = 0;
     // array utilizado {-2, 45, 0, 11, -9}
     for (int i = 0; i < count; i++) {
         // checar los elementos del array, limitado para no checar el ultimo elemento con el que sigue
-        for (int j = 0; j < (count - 1); j++) {
+        for (int j = i; j < count; j++) {
             // condicional para checar si es mayor un elemento que el otro
-            if(collection[j] > collection[j + 1]) {
+            if(*(collection + i) > *(collection + j)) {
                 // swap de elementos
-                int temp = *(collection + i);
-                *(collection + i) = *(collection + (j + 1));
-                *(collection + (j + 1)) = temp;
+                swapMade++;
+                int temp = *(collection + j);
+                *(collection + j) = *(collection + i);
+                *(collection + i) = temp;
             }
         }
     }
@@ -25,7 +27,7 @@ int bubble_sort(int *collection, int count)
     for (int k = 0; k < count; k++) {
         printf("from bubbleSort collection[%d] = %d\n", k, *(collection + k));
     }
-    return 0;
+    return swapMade;
 }
 
 void insertion_sort(int *collection, int count)
